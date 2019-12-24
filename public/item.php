14c270ccb2fc20f439a4a9eb2cf8c62177e5ec65
Clinton Reed <CREED0043@my.sullivan.edu>
2019-12-24 14:35:57 -0500

    more image link changes

--- public/item.php
+++ public/item.php
@@ -30,7 +30,7 @@
             <div class="row">
 
                 <div class="col-md-7">
-                    <img class="img-responsive" src="<?php echo $row['product_image']; ?>" alt="">
+                    <img class="img-responsive" src="../resources/uploads/<?php echo $row['product_image']; ?>" alt="">
 
                 </div>
 
